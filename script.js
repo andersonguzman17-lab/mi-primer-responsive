314fa2c06c7dd82b57f6a55aeae67735797c0adc
anderson guzman <andersonguzman17@gmail.com>
2026-07-21 18:48:04 -0500

    primer codigo java script

---
+++ script.js
@@ -0,0 +1,8 @@
+let edad_usuario = 16;
+
+if ( edad_usuario >= 18 ){
+    console.log (" eres mayor de edad, puedes ingtesar");
+
+} else {
+    console.log (" eres menor de edad, no puedes ingresar");
+}
